19b703ec612f78d33fc6b92abae0a17b02290e09
Eric London <git@ericlondon.com>
2014-01-24 21:00:17 -0500

    rails new rails4ajax

---
+++ rails4ajax/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4ajax::Application.config.secret_key_base = '71e065c0f4ec5f84dbe7fecf5eac658e03f97a79500699490968bff8e5c3d318d63f15497190ac6d0135105a664f737cbdde3f98fbe6c1f3012335a58f625554'
